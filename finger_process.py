ff72068a7ffbb0080e03c3587532922be4df295e
h3x4g0ns <agrawalekansh02@gmail.com>
2023-07-11 00:02:31 -0700

    digit follow

--- finger_process.py
+++ finger_process.py
@@ -4,20 +4,10 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
 cap = cv2.VideoCapture(0)
-MOMENTUM = 0.8
-
-def ramp(prev, point):
-    if not prev:
-        return point
-    else:
-        return int(MOMENTUM*prev[0]+(1-MOMENTUM)*point[0]), int(MOMENTUM*prev[1]+(1-MOMENTUM)*point[1])
 
 with mp_hands.Hands(
-    min_detection_confidence=0.75,
+    min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
-
-    # ramping function for smoothing
-    prev_left, prev_right = None, None
     
     while cap.isOpened():
         success, image = cap.read()
@@ -30,19 +20,19 @@ with mp_hands.Hands(
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         image_height, image_width, _ = image.shape
 
-        # ripping joints and handedness
         if results.multi_hand_landmarks:
-            for i, hand_landmarks in enumerate(results.multi_hand_landmarks):
-                hand_type = results.multi_handedness[i].classification[0].label
-                j = 8
-                pt = mp_drawing._normalized_to_pixel_coordinates(
-                        hand_landmarks.landmark[j].x, hand_landmarks.landmark[j].y, image_width, image_height)
-                if pt:
-                    if hand_type == "Left":
-                        pt = prev_left = ramp(prev_left, pt) 
-                    else:
-                        pt = prev_right = ramp(prev_right, pt)
-                    cv2.circle(image, pt, 5, (0, 255, 0), -1)
+            for hand_landmarks in results.multi_hand_landmarks:
+                finger_points = []
+                # joints
+                for i in range(5, 9):
+                    landmark_px = mp_drawing._normalized_to_pixel_coordinates(
+                        hand_landmarks.landmark[i].x, hand_landmarks.landmark[i].y, image_width, image_height)
+                    if landmark_px:
+                        cv2.circle(image, landmark_px, 5, (0, 255, 0), -1)
+                        finger_points.append(landmark_px)
+                # connections 
+                for i in range(len(finger_points)-1):
+                        cv2.line(image, finger_points[i], finger_points[i+1], (255, 0, 0), 2)
 
         cv2.imshow('Index finger Tracking', image)
         if cv2.waitKey(5) == 27:
