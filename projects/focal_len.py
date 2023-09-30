a24ac7803c00c2eeef4c1c19459f44a536014067
h3x4g0ns <agrawalekansh02@gmail.com>
2023-09-29 19:39:47 -0700

    cleanup

--- projects/focal_len.py
+++
@@ -1,9 +0,0 @@
-import math
-
-sensor_width = 4.8  # mm
-FOV_degrees = 78
-FOV_radians = math.radians(FOV_degrees)
-
-pixel_focal_length = (sensor_width * 0.5) / math.tan(FOV_radians * 0.5)
-
-print("Estimated pixel focal length:", pixel_focal_length)
