82b1f98695b3d9266201b9f1d8043aa5e6c14eef
h3x4g0ns <agrawalekansh02@gmail.com>
2023-11-07 15:43:21 -0800

    added framework for c2w

--- lib/world.py
+++ lib/world.py
@@ -14,14 +14,16 @@ class World:
     self.__setup()
 
   def __setup(self):
-    self.intrinsic = [self.FX, 0, self.width/2, 0, self.FY, self.height/2, 0, 0, 1]
-    self.intrinsic = np.asarray(self.intrinsic).reshape((3, 3)).astype(float)
-    self.intrinsic_inv = np.linalg.inv(self.intrinsic)
-    self.x, self.y = np.meshgrid(np.arange(self.width), np.arange(self.height))
-    self.x = self.x.flatten()
-    self.y = self.y.flatten()
+    intrinsic = [self.FX, 0, self.width/2, 0, self.FY, self.height/2, 0, 0, 1]
+    self.intrinsic = np.asarray(intrinsic, dtype=np.float32).reshape((3, 3))
+    self.i2c = np.linalg.inv(self.intrinsic).T
+    extrinsic = np.eye(4, dtype=np.float32)
+    self.c2w = extrinsic
+    x, y = np.meshgrid(np.arange(self.width), np.arange(self.height)).astype(np.float32)
+    self.x = x.flatten()
+    self.y = y.flatten()
 
-  def im2cam(self, depth_frame, step_size=16):
+  def to_cam(self, depth_frame, step_size=16):
     """
     Given monocular estimation of depth we want to take out coordinates in the 
     image space and project them to the camera space. This should give us a 3d 
@@ -30,9 +32,32 @@ class World:
     depth_values = depth_frame.flatten()
     x = np.multiply(self.x, depth_values)
     y = np.multiply(self.y, depth_values)
-    points = np.vstack((x, y, depth_values))[:, ::step_size]
-    camera_pts = (self.intrinsic_inv @ points).T
+    points = np.hstack((x, y, depth_values))[::step_size, :]
+    camera_pts = self.im2cam(points)
     return camera_pts
+
+  def im2cam(self, points):
+    """
+    Multiply image points by intrinsic matrix to get world coordinates
+    """
+    return points @ self.i2c
+  
+  def to_world(self, depth_frame, step_size=16):
+    """
+    TODO:
+    """
+    camera_pts = self.to_cam(depth_frame, step_size)
+    world_pts = self.cam2world(self, camera_pts)
+    return world_pts
+
+  def cam2world(self, points):
+    """
+    TODO:
+    """
+    ones = np.ones((points.shape[0], 1), dtype=np.float32)
+    points = np.hstack((points, ones))
+    world_pts = (points @ self.c2w)[:, :3]
+    return world_pts
 
 if __name__ == "__main__":
   from depth import get_depth
@@ -43,27 +68,35 @@ if __name__ == "__main__":
 
   cap = cv2.VideoCapture(-1)
   world = World(height=480, width=640)
+  H, W, K, c2w = world.height, world.width, world.intrinsic, world.c2w
   server = viser.ViserServer(share=True)
   step_size = 16
+  wxyz = viser.transforms.SO3.from_matrix(c2w[:3, :3]).wxyz
+  position = c2w[:3, 3],
 
+  # here we transform all pixels coordinates from the image
+  # in production, we only need to project the points of the bounding box of the cat
   while True:
     start = time.time()
     ret, frame = cap.read()
     depth = get_depth(frame) * 0.005
     pts = world.im2cam(depth, step_size=16)
-    H, W, K = world.height, world.width, world.intrinsic
     colors = np.transpose(frame, axes=(1, 0, 2)).reshape(-1, 3)
     colors = colors[::step_size, :]
     fps = 1.0 / (time.time() - start)
     sys.stdout.write("\rFPS: {:.2f}".format(fps))
     sys.stdout.flush()
 
+    # adds viewport
+    # TODO: need to add the extrinsic matrix for proper pose
     server.add_camera_frustum(
       "/view",
       fov=2 * np.arctan2(H / 2, K[0, 0]),
       aspect=W/H,
       scale=0.25,
       image=frame,
+      wxyz=wxyz,
+      position=position,
     )
 
     server.add_point_cloud(
@@ -71,6 +104,8 @@ if __name__ == "__main__":
       points=pts,
       colors=colors,
       point_size=0.01,
+      wxyz=wxyz,
+      position=position,
     )
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
