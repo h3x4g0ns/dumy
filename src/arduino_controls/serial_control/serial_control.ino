ea9fbc9ae75d507d725a365925b349fb46dd2bc7
JayHil <jaydenhilton8@gmail.com>
2023-11-07 23:34:09 -0800

    fixed overwrite and issue with negatives

--- src/arduino_controls/serial_control/serial_control.ino
+++ src/arduino_controls/serial_control/serial_control.ino
@@ -9,17 +9,27 @@ Servo l1;
 Servo l2;
 Servo l3;
 
+int i = 0;
+char data[16];
+bool terminated;
+
 float strlfcopy(const char *src, size_t size) {
     char dst[size];
     int i = 0;
     float res;
+    int n = 1;
+    if (src[0] == '-') {
+        *src++;
+        n = -1;
+    }
     while (size > 1 && *src) {
         dst[i] = *src++;
         i++;
         size--;
     }
     res = atof(dst);
-    return res;
+    Serial.println(dst);
+    return res * n;
 }
 
 void setup() {
@@ -28,23 +38,32 @@ void setup() {
     l2.attach(L2_PORT);
     l3.attach(L3_PORT);
     Serial.println("init");
+    terminated = false;
+    data[0] = '\0';
 }
 
 void loop() {
-    //Serial input
-    int incoming = Serial.available();
-    if (incoming==9) {
-        char data[incoming];
+    //read in serial data until the terminating character
+    if (Serial.available()>0) {
+        char incoming = Serial.read();
+        if (incoming == 'r') {
+            terminated = true;
+            i = 0;
+        } else {
+            data[i] = incoming;
+            i++;
+        }
+    }
+
+    if (terminated) {
         float angles[3];
-        //read in serial data
-        Serial.readBytesUntil("\n", data, incoming);
-        //split string and convert to float
         angles[0] = strlfcopy(strtok(data, ","), 5);
-        angles[1] = strlfcopy(strtok(data, ","), 5);
-        angles[2] = strlfcopy(strtok(data, ","), 5);
-        Serial.println(angles[0]);
+        angles[1] = strlfcopy(strtok(NULL, ","), 5);
+        angles[2] = strlfcopy(strtok(NULL, ","), 5);
         l1.write(angles[0]);
         l2.write(angles[1]);
         l3.write(angles[2]);
+        memset(data, 0, 16);
+        terminated = false;
     }
 }
