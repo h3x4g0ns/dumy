5ee0ad4f107b5785bef1ce1b916931e121cdd3f7
Ekansh Agrawal <agrawalekansh02@gmail.com>
2023-11-30 22:49:24 -0800

    reading serial communication from server (#6)
    * partially complete serial control code
    * minor change to serial_control
    * removed linked list
    * fixed serial_control
    * how do i use named methods in python lol
    * fixed overwrite and issue with negatives
    * wrong terminator
    * switching
    * main changes
    * initial move to world test, might need to add delays to let serial coms catch up
    * added stls for updated arm
    * adjusted stls
    * issue with serial coms again maybe?
    * radian to degrees conversion lol
    * ik check + serial coms
    
    Co-authored-by: JayHil <jaydenhilton8@gmail.com>

--- lib/robot.py
+++ lib/robot.py
@@ -1,5 +1,6 @@
 import numpy as np
 import serial
+from time import sleep
 
 class Robot(object):
     """Robot class for controlling and defining robot arm"""
@@ -49,7 +50,7 @@ class Robot(object):
         """
         c2 = (np.linalg.norm(xd)**2 - l1**2 - l2**2)/(2*l1*l2)
         ret = {}
-        if c2 > 1:
+        if np.abs(c2) > 1:
             ret["0"] = np.array([np.nan, np.nan])
         elif c2 == 1:
             ret["1"] = np.array([np.arctan2(xd[1], xd[0]), 0])
@@ -77,9 +78,9 @@ class Robot(object):
         """
         sols = self.rik(self.l2, self.l3, (np.sqrt(xd[0]**2 + xd[1]**2), -xd[2]+self.l1))
         key = "1" if "1" in sols else "0"
-        q1 = np.arctan2(xd[1], xd[0])
-        q2 = sols[key][0]
-        q3 = sols[key][1]
+        q1 = np.rad2deg(np.arctan2(xd[1], xd[0]))
+        q2 = np.rad2deg(sols[key][0])
+        q3 = np.rad2deg(sols[key][1])
         return np.array([q1, q2, q3])
 
     def joint2world(self, q):
@@ -109,17 +110,53 @@ class Robot(object):
         Returns:
             bool: True if successful, False otherwise
         """
-        q = list(self.world2joint(xd))
+        q = self.world2joint(xd)
         if np.isnan(q[0]) or np.isnan(q[1]) or np.isnan(q[2]):
             return False
-        if not self.prev_state:
+        if self.prev_state is None:
             self.prev_state = q
         self.prev_state = self.ramp(self.prev_state, q)
-        data = ",".join(map(str, self.prev_state)) + "\n"
+        data = ",".join(map(str, q)) + "r"
         self.connec.write(data.encode())
+        print(data)
         return True
+
+    def move_base(self, xd):
+        """
+        Moves base of the robot given a destination coordinate
+
+        Args:
+            xd (np.array): world coordinates (x, y, z)
+        """
+        # q = np.arctan2(xd[1], xd[0])/np.pi*180
+        q = str(int(xd)) + ",0,0r"
+        print(q)
+        self.connec.write(q.encode())
+
+# Rough Measurements from arm:
+# l1 = 4.13cm
+# l2 = 12cm
+# l3 = 12cm
 
 class Dummy(Robot):
     def __init__(self, port):
         """Configures 3DOF robot for Dummy"""
-        super().__init__(l1=1, l2=1, l3=1, port=port)
+        super().__init__(l1=4.13, l2=12, l3=12, port=port)
+
+def main():
+    port = "COM3"
+    coords = [0, -90, 0, 90, 0]
+    with Dummy(port) as d:
+        for coord in coords:
+            d.move_base(coord)
+            sleep(3)
+
+    # coords = np.array([[0,0,0], [18, 0, 20]])
+    # with Dummy(port) as d:
+    #     for i in range(0, len(angles)):
+    #         xd = angles[i]
+    #         d.move(xd)
+    #         sleep(2)
+
+if __name__ == "__main__":
+    main()
