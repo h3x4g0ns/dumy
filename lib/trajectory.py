f843b66699e871012b3f186cdf6bc12186c73a4c
h3x4g0ns <agrawalekansh02@gmail.com>
2023-12-10 18:07:49 -0800

    more prgress on trajectory

--- lib/trajectory.py
+++ lib/trajectory.py
@@ -1,4 +1,5 @@
 from sympy import symbols, cos, sin, pi, simplify, solve
+import numpy as np
 
 # Define symbolic variables
 # theta1, theta2, theta3 = symbols('theta1 theta2 theta3')
@@ -13,11 +14,24 @@ class Trajectory:
         self.l2 = robot.l2
         self.l3 = robot.l3
         self.prev_cat_pos = None
+        self.cat_pos_avg = None
 
-    def trajectory_engine(self):
+    def calculate(self, cat_pos):
         # Forward kinematics equations (position of the end effector)
-        x, y, z = self.robot.joint2world(self.robot.prev_state)
-        desired_x, desired_y = self.robot.endEffectorToWorld()
+        x, y= self.robot.joint2world(self.robot.prev_state)
+
+        #find new desired laser pos
+        laserPos = cat_pos + np.linalg.norm(cat_pos - self.prev_cat_pos)*20
+        desired_x, desired_y = self.robot.endEffectorToWorld(laserPos)
+
+        #calculate cat activation
+        if (self.cat_pos_avg == None):
+            #send code for piezo buzzer
+            self.cat_pos_avg = cat_pos
+            self.prev_cat_pos = cat_pos
+        else:
+            self.cat_pos_avg = (self.cat_pos_avg + cat_pos) / 2
+            self.prev_cat_pos = cat_pos
 
         # Solve inverse kinematics equations to find joint angles
         inverse_kinematics_equations = [
--- tasks/dumy/main.py
+++ tasks/dumy/main.py
@@ -1,6 +1,8 @@
 from lib.depth import get_depth
 from lib.detect import get_cat_mask, apply_mask_to_image
 from lib.world import World
+from lib.robot import Dumy
+from lib.trajectory import Trajectory
 import cv2
 import time
 import sys
@@ -29,47 +31,55 @@ def main():
   # setting up world
   world = World(height=480, width=640, render="SHOW" in os.environ)
   step_size = 16
-  
 
-  while True:
+  # setting up robot and engine
+  PORT = "COM3"
+  with Dumy(PORT) as d:
+    engine = Trajectory(d)
+    while True:
 
-    ret, frame = cap.read()
-    start_time = time.time()
+      ret, frame = cap.read()
+      start_time = time.time()
 
-    # get depth and cat bounding boxes
-    detect = get_cat_mask(frame)
-    depth = get_depth(frame) * 0.005
+      # get depth and cat bounding boxes
+      detect = get_cat_mask(frame)
+      depth = get_depth(frame) * 0.005
+
+      if "SHOW" in os.environ:
+        # only showing concated frames with proper env var
+        detect = apply_mask_to_image(frame, detect)
+        depth = cv2.cvtColor(depth, cv2.COLOR_GRAY2BGR)
+        frame = combine_frames(depth, detect)
+        cv2.imshow("detect and depth", frame)
+        
+        # we can also show the world view
+        world.to_world(depth, step_size)
+
+      # get world coordinate for cat which is middle of bounding box
+      if len(detect) > 0:
+        x1, y1, x2, y2 = detect[0]
+        x = (x1 + x2) // 2
+        y = (y1 + y2) // 2
+        cat_pos = world.transform(np.array([[x, y]]), depth, frame)
+
+      # timing fps and printing w/ carriage return
+      end_time = time.time()
+      fps = 1.0 / (end_time - start_time)
+      sys.stdout.write("\rFPS: {:.2f}".format(fps))
+      sys.stdout.flush()
+
+      # move robot to world coordinate
+      if len(cat_pos) > 0:
+        joint_angles = engine.calculate(cat_pos)
+        d.move(joint_angles)
+
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    cap.release()
 
     if "SHOW" in os.environ:
-      # only showing concated frames with proper env var
-      detect = apply_mask_to_image(frame, detect)
-      depth = cv2.cvtColor(depth, cv2.COLOR_GRAY2BGR)
-      frame = combine_frames(depth, detect)
-      cv2.imshow("detect and depth", frame)
-      
-      # we can also show the world view
-      world.to_world(depth, step_size)
-
-    # get world coordinate for cat which is middle of bounding box
-    if len(detect) > 0:
-      x1, y1, x2, y2 = detect[0]
-      x = (x1 + x2) // 2
-      y = (y1 + y2) // 2
-      world_coords = world.transform(np.array([[x, y]]), depth, frame)
-
-    # timing fps and printing w/ carriage return
-    end_time = time.time()
-    fps = 1.0 / (end_time - start_time)
-    sys.stdout.write("\rFPS: {:.2f}".format(fps))
-    sys.stdout.flush()
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-
-  cap.release()
-
-  if "SHOW" in os.environ:
-    cv2.destroyAllWindows()
+      cv2.destroyAllWindows()
 
 if __name__ == "__main__":
   main()
